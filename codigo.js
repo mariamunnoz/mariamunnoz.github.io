623ffc5a1d08754d72b3c8bb06e1898c5fa957d8
mariamunnoz <90861192+mariamunnoz@users.noreply.github.com>
2021-11-11 19:06:57 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,1 +1,6 @@
-
+function addItem(){
+  const cantidad = document.getElementById("input").value
+  const tipo = document.getElementById("options").value
+  console.log(cantidad)
+  console.log(tipo)
+}
