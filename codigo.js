e3a8226faffe918892790fa2815d9af05f713525
mariamunnoz <90861192+mariamunnoz@users.noreply.github.com>
2021-11-11 19:24:08 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -3,4 +3,11 @@ function addItem(){
   const tipo = document.getElementById("options").value
   console.log(cantidad)
   console.log(tipo)
+  const fila = document.createElement("tr")
+  const c1 = document.createElement("td")
+  c1.innerText = cantidad
+  const c2=document.createElement("td")
+  c2.innerText = tipo
+  fila.appendChild(c1)
+  fila.appendChild(c2)
 }
